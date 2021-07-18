e57886653a38ab4e4d802248b38303aa9d0d0ab7
iJonna <ijonna@MacBook-Pro-de-iJonna.local>
2021-07-18 16:25:50 -0500

    uploading project

---
+++ app-library/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from '@/views/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+<style lang="scss">
+#app {
+  text-align: center;
+  background-color: beige;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ app-library/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+
+      <div class="navbar-start" style="flex-grow: 1; justify-content: center">
+        <a class="navbar-item" href="/">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
